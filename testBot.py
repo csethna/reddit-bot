45765a67b8775d7fc6425f8af6bfebc8b960bca8
Cyrus Sethna <csethna@gmail.com>
2017-08-17 10:13:58 -0500

    testBot monitors Cavs
    Point bot to monitor comments section of `r/ClevelandCavs` and reply to
    comments mentioning `LeBron` with `Believeland, u/{0}`

--- testBot.py
+++ testBot.py
@@ -1,9 +1,24 @@
 import praw
 from credentials import *
 
+# Authentications
 bot = praw.Reddit(user_agent='testBot v0.1',
-client_id=CLIENT_ID
-client_secret=CLIENT_SECRET
-username=USERNAME
-password=PASSWORD
-)
+client_id = CLIENT_ID,
+client_secret = CLIENT_SECRET,
+username = USERNAME,
+password = PASSWORD)
+
+# Pointing testBot to r/clevelandcavs
+subreddit = bot.subreddit('clevelandcavs')
+
+# Monitor r/clevelandcavs for new comments with generator
+comments = subreddit.stream.comments()
+
+for comment in comments:
+    text = comment.body # Fetch body
+    author = comment.author # Fetch author
+    if 'lebron' in text.lower():
+        # Generate message
+        message = "BelieveLand, u/{0}".format(author)
+
+        comment.reply(message) # Send message
